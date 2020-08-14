1b6ea7d1665f303fbbfe86e3156aabba96316af9
NourNiazy <nourmostafa01551@gmail.com>
2020-08-14 06:22:26 +0300

    ....

--- barber/js/jquery.js
+++ barber/js/jquery.js
@@ -1,17 +1,17 @@
-$('.owl-carousel').owlCarousel({
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
     loop:true,
     margin:0,
+    autoplay:true,
+    autoplayTimeout:5000,
     nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
 })
 
